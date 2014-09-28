3507b61b1cacd512b7ee524451870d214a87ceeb
Jan Bernitt <jan.bernitt@gmx.de>
2014-09-28 20:34:32 +0200

    initial commit

---
+++ src/frac64/FRAC64.java
@@ -0,0 +1,105 @@
+package frac64;
+
+import static java.lang.Math.abs;
+
+/**
+ * 
+ * 00-30: 31 bit unsigned denominator
+ * 31-31: zero bit (unused sign)
+ * 32-63: 32 bit signed numerator
+ *
+ */
+public final class FRAC64 {
+
+	static long frac(int numerator, int denominator) {
+		if (denominator < 0)
+			numerator = -numerator;
+		return ((long)numerator << 32) | abs(denominator);
+	}
+	
+	static void show(long v) {
+		System.out.println(String.format("%64s", Long.toBinaryString(v)));
+	}
+	
+	static boolean isNaN(long frac64) {
+		return denominator(frac64) == 0;
+	}
+	
+	static long add(long left, long right) {
+		int l_d = denominator(left);
+		int r_d = denominator(right);
+		if (l_d == r_d) // fast path for equal d's
+			return cancel( left + right - l_d ); // one of the equal d's has been added to much, subtract it again
+		long l_n = (left >> 32) * r_d;
+		long r_n = (right >> 32) * l_d;
+		long d = (long)r_d * l_d;
+		// overflow checks
+		return cancel( ((l_n+r_n) << 32) + (int)d);
+	}
+	
+	static long sub(long left, long right) {
+		return add(left, neg(right));
+	}
+	
+	static long neg(long frac64) {
+		return (-(frac64 >> 32) << 32) | (int)frac64;
+	}
+	
+	static long mul(long left, long right) {
+		long n = ((left >> 32) * (right >> 32)) << 32;
+		long d = (int)left * (int)right;
+		return cancel(n | d);
+	}
+	
+	static long div(long left, long right) {
+		long n = ((left >> 32) * (int)right) << 32;
+		long d = (int)left * (right >> 32);
+		if (d < 0) {
+			d = -d;
+			n = -n;
+		}
+		return cancel(n | d);
+	}
+	
+	private static long cancel(long frac64) {
+		int n = numerator(frac64);
+		if (n == 1 || n == -1)
+			return frac64;
+		int d = denominator(frac64); 
+		int gcd = gcd(abs(n), d);
+		return frac(n / gcd, d / gcd); //TODO not use frac that allows/checks for negative d
+	}
+	
+	//TODO cancel for n + d args 
+	
+	private static int gcd(int a, int b) {
+		while (b != 0) {
+			int t = b;
+			b = a % b;
+			a = t;
+		}
+		return a;
+	}
+	
+	static int numerator(long frac64) {
+		return (int) (frac64 >> 32);
+	}
+	
+	static int denominator(long frac64) {
+		return (int)frac64;
+	}
+	
+	static String print(long frac64) {
+		int d = denominator(frac64);
+		if (d <= 0)
+			return "NaN";
+		int n = numerator(frac64);
+		if (n == 0)
+			return "0";
+		if (abs(n) == d)
+			return Integer.signum(n)+"";
+		if (d == 1)
+			return String.valueOf(n);
+		return n+"/"+d;
+	}
+}
---
+++ src/frac64/TestFRAC64.java
@@ -0,0 +1,71 @@
+package frac64;
+
+import static frac64.FRAC64.add;
+import static frac64.FRAC64.div;
+import static frac64.FRAC64.frac;
+import static frac64.FRAC64.mul;
+import static frac64.FRAC64.print;
+import static frac64.FRAC64.sub;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestFRAC64 {
+
+	@Test
+	public void fractionFromNumeratorAndDenominator() {
+		assertFraction("1/2", frac(1, 2));
+		assertFraction("3/4", frac(3, 4));
+		assertFraction("1", frac(3, 3));
+		assertFraction("-1", frac(3, -3));
+		assertFraction("-1", frac(-3, 3));
+	}
+	
+	@Test
+	public void addition() {
+		assertFraction("137/72", add(frac(17, 18), frac(23, 24)));
+		assertFraction("3/2", add(frac(1, 2), frac(1, 1)));
+		assertFraction("1", add(frac(3, 4), frac(1, 4)));
+		assertFraction("5/8", add(frac(3, 8), frac(2, 8)));
+		assertFraction("3/4", add(frac(1, 2), frac(1, 4)));
+		assertFraction("17/35", add(frac(1, 5), frac(2, 7)));
+		assertFraction("-3/35", add(frac(1, 5), frac(-2, 7)));
+		assertFraction("1/5", add(frac(-2, 5), frac(3, 5)));
+		assertFraction("-1/5", add(frac(2, 5), frac(3, -5)));
+	}
+	
+	@Test
+	public void subtraction() {
+		assertFraction("-1/2", sub(frac(1, 2), frac(1, 1)));
+		assertFraction("1/2", sub(frac(3, 4), frac(1, 4)));
+		assertFraction("1/4", sub(frac(1, 2), frac(1, 4)));
+		assertFraction("-3/35", sub(frac(1, 5), frac(2, 7)));
+		assertFraction("17/35", sub(frac(1, 5), frac(-2, 7)));
+		assertFraction("-1", sub(frac(-2, 5), frac(3, 5)));
+		assertFraction("1", sub(frac(2, 5), frac(3, -5)));
+		assertFraction("1/8", sub(frac(3, 8), frac(2, 8)));
+		assertFraction("-1/72", sub(frac(17, 18), frac(23, 24)));
+	}
+	
+	@Test
+	public void multiplication() {
+		assertFraction("-1/16", mul(frac(3, 6), frac(-1, 8)));
+		assertFraction("1/8", mul(frac(1, 2), frac(1, 4)));
+		assertFraction("1/16", mul(frac(3, 6), frac(1, 8)));
+	}
+	
+	@Test
+	public void division() {
+		assertFraction("-4", div(frac(3, 6), frac(-1, 8)));
+		assertFraction("2", div(frac(1, 2), frac(1, 4)));
+		assertFraction("4", div(frac(3, 6), frac(1, 8)));
+		assertFraction("8/5", div(frac(2, 5), frac(1, 4)));
+		assertFraction("-8/5", div(frac(2, 5), frac(1, -4)));
+		assertFraction("8/5", div(frac(-2, 5), frac(1, -4)));
+		assertFraction("-8/5", div(frac(-2, 5), frac(1, 4)));
+	}
+	
+	private static void assertFraction(String expected, long actual) {
+		assertEquals(expected, print(actual));
+	}
+}
